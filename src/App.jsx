70757729075e6e6cd518280e1a90856aab2c6ec3
Samuel Mburu <sammxuser@gmail.com>
2024-01-09 08:53:32 +0300

    Ability to X each square on the board

--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,40 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-export default function App() {
-  // const [count, setCount] = useState(0)
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue('X');
+  }
+  return (
+    <button className='square' onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  // const [squares, setSquares] = useState(Array(9).fill(null));
 
   return (
     <>
       <div className='board-row'>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div className='board-row'>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
+        <Square value='' />
+        <Square value='' />
+        <Square value='' />
       </div>
 
       <div className='board-row'>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
+        <Square value='' />
+        <Square value='' />
+        <Square value='' />
       </div>
-
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   );
 }
